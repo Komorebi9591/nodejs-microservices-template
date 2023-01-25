fd69a009d7b71364403a2f0b8a40d13109965be0
Komorebi9591 <85187555+Komorebi9591@users.noreply.github.com>
2023-01-25 14:49:09 +0000

    wip

--- packages/gateway-api/app.js
+++ packages/gateway-api/app.js
@@ -3,13 +3,17 @@ const cookieParser = require('cookie-parser');
 const pino = require('pino-http')();
 
 const router = require('./routes/index');
+const auth = require('./middlewares/auth');
 
 const app = express();
+
 
 app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
+
 
 app.use('/api', router);
 
